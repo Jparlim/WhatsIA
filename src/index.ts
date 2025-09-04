f3730efc2bb24c49b1ee9bbb0b3af498c90dded2
Jparlim <Gusjao0500@gmail.com>
2025-09-04 18:53:27 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,27 @@ import fastify from "fastify";
 import cors from "@fastify/cors"
 import whatsappRoutes from "./routes/whatsapp";
 
-const App = fastify();
+
+const App = fastify({logger:true});
+App.register(import("@fastify/formbody"))
 
 App.register(cors, { origin: "*"})
 App.register(whatsappRoutes);
+
+interface data {
+    Body: Object,
+    From: Number
+}
+
+App.post('/whatsapp', async (request, reply) => {
+    const { Body, From } = request.body as data
+
+    console.log("mensagem recebida", Body)
+    console.log("mensagem recebida", From)
+
+    reply.header("content-type", "text/xml");
+    reply.status(200).send({message: `recebemos sua mensagem${Body}`})
+})
+
 
 App.listen({port:3000}, () => console.log("servidor filhadaputamente rodando!"))
